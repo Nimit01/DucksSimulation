b35faedc493ba5f0cc277ec663b7a6ebdc2148a3
Nimit Prakash <nimitprakash07@gmail.com>
2025-03-08 23:56:12 +0530

    Added Main.java with menu-driven Duck simulation

---
+++ Main.java
@@ -0,0 +1,33 @@
+// Name - Nimit Prakash
+//Prn - 23070126082
+// AIML B1
+
+public class Main {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
+        mallard.swim();
+
+        System.out.println();
+
+        Duck rubber = new RubberDuck();
+        rubber.display();
+        rubber.performFly();
+        rubber.performQuack();
+        rubber.swim();
+
+        System.out.println();
+
+        Duck decoy = new DecoyDuck();
+        decoy.display();
+        decoy.performFly();
+        decoy.performQuack();
+        decoy.swim();
+
+        System.out.println("\nTesting behavior change at runtime:");
+        mallard.setFlyBehavior(new FlyNoWay());
+        mallard.performFly();
+    }
+}
